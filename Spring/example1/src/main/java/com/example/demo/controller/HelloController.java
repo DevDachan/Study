114430dc8f0b84b6216b9424849077abcb6e8759
DevDachan <dachan9524@gmail.com>
2023-01-19 20:50:20 +0900

    Add Spring Example

---
+++ Spring/example1/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   // @RequestMapping(value="/hello", method= RequestMethod.GET)
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world\n";
+    }
+}
