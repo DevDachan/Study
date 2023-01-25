2b3a4ae98ade7f2ff68bb17fa2e4eea537653dec
DevDachan <dachan9524@gmail.com>
2023-01-25 17:48:15 +0900

    Add 13 Spring Study Note

--- Spring/example1/src/main/java/com/example/demo/controller/ProductController.java
+++ Spring/example1/src/main/java/com/example/demo/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
 import com.example.demo.data.dto.ProductDTO;
+import com.example.demo.data.entity.ProductEntity;
 import com.example.demo.data.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.slf4j.Logger;
@@ -30,6 +34,7 @@ public class ProductController {
         return productDTO;
     }
 
+    /* Validation 적용 이전
     @PostMapping
     public ProductDTO createProduct(@RequestBody ProductDTO productDto) {
         String productId = productDto.getProductId();
@@ -37,6 +42,16 @@ public class ProductController {
         int productPrice = productDto.getProductPrice();
         int productStock = productDto.getProductStock();
         return productService.saveProduct(productId, productName, productPrice, productStock);
+    }*/
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDto) {
+        String productId = productDto.getProductId();
+        String productName = productDto.getProductName();
+        int productPrice = productDto.getProductPrice();
+        int productStock = productDto.getProductStock();
+        ProductDTO response = productService.saveProduct(productId,productName,productPrice,productStock);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
     @DeleteMapping(value = "/product.{productId}")
--- Spring/example1/src/main/java/com/example/demo/data/dto/ProductDTO.java
+++ Spring/example1/src/main/java/com/example/demo/data/dto/ProductDTO.java
@@ -1,6 +1,9 @@
 package com.example.demo.data.dto;
 
 import com.example.demo.data.entity.ProductEntity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Data
@@ -9,9 +12,17 @@ import lombok.*;
 @ToString
 @Builder
 public class ProductDTO {
+    @NotNull
     private String productId;
+    @NotNull
     private String productName;
+    @NotNull
+    @Min(value = 500)
+    @Max(value = 30000)
     private int productPrice;
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 9999)
     private int productStock;
 
     public ProductEntity toEntity(){
